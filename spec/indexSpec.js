9c8114258468ee7d6080f7d838b305755166fe53
kghugo <howtostudycantonese@gmail.com>
2020-07-23 16:43:02 +0800

    :white_check_mark: Test render()

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,5 +1,22 @@
+import { Paragraph } from '../src/index.js'
+
 describe('Test block essential functionalities', () => {
-  it('should render node', () => {
+  const paragraphBlock = new Paragraph()
+
+  const i18n = {
+    locale: 'en-US',
+    rtl: 'auto'
+  }
+
+  it('should return an node without data with render()', () => {
+    expect(paragraphBlock.render(i18n)).toHaveClass('freedom-editor-blocks')
+  })
+
+  /* it('should return an node with data with render()', () => {
 
   })
+
+  it('should save content of a node with save()', () => {
+
+  }) */
 })
